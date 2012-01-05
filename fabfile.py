cbf3b3504ee7d3544b8578dc57d2e0282356dd3b
Seh Hui, Leong <felixleong@gmail.com>
2012-01-05 15:47:23 +0800

    Added paginated listing template, fabric script and theme template.

---
+++ fabfile.py
@@ -0,0 +1,37 @@
+from fabric.api import *
+import os
+import fabric.contrib.project as project
+
+PROD = 'sl'
+DEST_PATH = '/var/www/stevelosh.com/'
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
+
+def clean():
+    local('rm -rf ./deploy')
+
+def generate():
+    local('hyde gen')
+
+def regen():
+    clean()
+    generate()
+
+def serve():
+    local('hyde -w -s . -k')
+
+def reserve():
+    regen()
+    serve()
+
+def smush():
+    local('smusher ./media/images')
+
+@hosts(PROD)
+def publish():
+    regen()
+    project.rsync_project(
+        remote_dir=DEST_PATH,
+        local_dir=DEPLOY_PATH.rstrip('/') + '/',
+        delete=True
+    )
