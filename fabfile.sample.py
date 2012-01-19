a104b1178eeb538e0747a05006fe6521c7ba9a88
Seh Hui, Leong <felixleong@gmail.com>
2012-01-19 16:56:09 +0800

    Added image upload command.

--- fabfile.sample.py
+++ fabfile.sample.py
@@ -1,11 +1,14 @@
-from fabric.api import *
 import os
+import op as op
+import sys
+from fabric.api import *
 import fabric.contrib.project as project
 
-PROD = 'sl'
-DEST_PATH = '/var/www/stevelosh.com/'
-ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
+PROD = [ ]  # Set up the SSH URL settings here
+DEST_PATH = '/var/www/website/'
+ROOT_PATH = op.abspath(op.dirname(__file__))
+DEPLOY_PATH = op.join(ROOT_PATH, 'deploy')
+IMG_PATH = '/var/www/website/deploy/media/'
 
 def clean():
     local('rm -rf ./deploy')
@@ -36,3 +39,17 @@ def publish():
         local_dir=DEPLOY_PATH.rstrip('/') + '/',
         delete=True
     )
+
+@hosts(PROD)
+def uploadimage(filename):
+    if not op.isfile(filename):
+        print >> sys.stderr, 'ERROR: File does not exists'
+        return
+
+    today = datetime.now()
+    upload_dir= op.join(IMG_PATH, '{0}/{1}/'.format(today.year, today.month))
+    upload_file = op.join(upload_dir, op.basename(filename))
+    mkdir_cmd = 'mkdir -p {0}'.format(upload_dir)
+
+    run(mkdir_cmd)
+    put(filename, upload_dir)
