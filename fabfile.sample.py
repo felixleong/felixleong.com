f90c75ac45ae06b5b657869982e98ab6e101da9b
Seh Hui, Leong <felixleong@gmail.com>
2013-08-18 13:39:58 +0800

    Updated Fabric file.

--- fabfile.sample.py
+++ fabfile.sample.py
@@ -1,17 +1,19 @@
-import os
 import os.path as op
-import sys
-from fabric.api import *
+from fabric.api import env, hosts, local
 import fabric.contrib.project as project
 
-PROD = [ ]  # Set up the SSH URL settings here
+env.use_ssh_config = True
+
+PROD = []  # Set up the SSH URL settings here
 DEST_PATH = '/var/www/website/'
 ROOT_PATH = op.abspath(op.dirname(__file__))
 DEPLOY_PATH = op.join(ROOT_PATH, 'deploy')
 IMG_PATH = '/var/www/website/deploy/media/'
 
+
 def clean():
     local('rm -rf ./deploy')
+
 
 def generate(type='development', gen_dir=None):
     gen_cmd = 'hyde gen'
@@ -21,19 +23,24 @@ def generate(type='development', gen_dir=None):
     local('sed -e \'s/\(mode: \).*/\\1{0}/g\' -i\'\' site.yaml'.format(type))
     local(gen_cmd)
 
+
 def regen(type='development'):
     clean()
     generate(type)
 
+
 def serve():
     local('hyde serve')
+
 
 def reserve():
     regen()
     serve()
 
+
 def smush():
     local('smusher ./media/images')
+
 
 @hosts(PROD)
 def publish():
@@ -41,19 +48,6 @@ def publish():
     project.rsync_project(
         remote_dir=DEST_PATH,
         local_dir=DEPLOY_PATH.rstrip('/') + '/',
+        extra_opts="--checksum",
         delete=True
     )
-
-@hosts(PROD)
-def uploadimage(filename):
-    if not op.isfile(filename):
-        print >> sys.stderr, 'ERROR: File does not exists'
-        return
-
-    today = datetime.now()
-    upload_dir= op.join(IMG_PATH, '{0}/{1}/'.format(today.year, today.month))
-    upload_file = op.join(upload_dir, op.basename(filename))
-    mkdir_cmd = 'mkdir -p {0}'.format(upload_dir)
-
-    run(mkdir_cmd)
-    put(filename, upload_dir)
